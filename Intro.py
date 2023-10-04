fd16bbafc41e8c814a3fe6b5c870212b23bf1d03
Deepak <deepakyadav540official@gmail.com>
2023-10-04 23:43:43 +0530

    3rd commit

--- Intro.py
+++ Intro.py
@@ -1,1 +1,25 @@
 print("Hello World !!")
+print(5)
+print("Bye, I will come alone")
+print(12*17)
+print("Hey I am a good boy \nI like to \"eat pizza\"\nI don't like burger \nI can go over taking hotdogs")
+
+
+# \n : Escape Sequence character
+# # : single line comment
+# """ lines """ : Multiple line comments
+# cltr + / --> short cut to comment
+
+""" 
+print("Hey are your there")
+print("Something is wrong")
+print("I know the hack behind it")
+
+"""
+
+# Select all and cltr + / or """ ---  """
+# Alt + ^ or (down arrow key) to place the sentences inside quotes.
+
+print("Hey" , 6, 7)
+print("Hey" , 6, 7, sep='~', end="0.007\n")
+print("harry met Sejal")
